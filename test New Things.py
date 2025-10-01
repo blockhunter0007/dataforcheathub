56913ad6d8d4f2d0ee9abaeec09e2ab402400d7f
blockhunter0007 <a23122160@gmail.com>
2025-10-01 19:15:00 +0200

    Update test New Things.py

--- test New Things.py
+++ test New Things.py
@@ -11,8 +11,10 @@ version = "01.00.00"
 ############
 
 
-
+daten = {}
+json_load = False
 dateipfad = Path(__file__).parent / "data" / "server.json"
+script_ordner = Path(__file__).parent
 url = 'https://blockhunter0007.github.io/cheathubdumpingground/server.json'
 response = requests.get(url)
 if response.status_code == 200:
@@ -24,38 +26,22 @@ else:
     print("Fehler beim Abrufen:", response.status_code)
     json_load = False
 
-
 #json_string = ''
 #daten = json.loads(json_string)
 #print(daten["Hund"])   # Hund
-
-script_ordner2 = os.path.dirname(os.path.abspath(__file__))
-script_ordner = Path(__file__).parent
-if script_ordner != script_ordner2:
-    print(script_ordner)
-    print(script_ordner2)
-    time.sleep(20)
-else:
-    print('pass test folder load')
 
 if daten["version"] != version:
     print('update')
     print('your version', version, 'is not the newest its', daten["version"])
 # Pfad zur server.json im Unterordner "data"
 if json_load == False:
-    offline_start()
-
-
-def offline_start():
-    json_datei = script_ordner / "data" / "server.json"
 # Prüfen, ob die Datei existiert
-    if not json_datei.exists():
-        print(f"Datei nicht gefunden: {json_datei}")
+    if not dateipfad.exists():
+        print(f"Datei nicht gefunden: {dateipfad}")
         exit(1)
 # JSON-Datei laden
-    with open(json_datei, "r", encoding="utf-8") as f:
+    with open(dateipfad, "r", encoding="utf-8") as f:
         daten = json.load(f)
-
 # Beispielzugriff auf die Daten
     print("Geladene Daten:")
     print(daten)
