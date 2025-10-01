7efae91deb0b3a40374469d810e3cab2ff063899
blockhunter0007 <a23122160@gmail.com>
2025-10-01 18:30:32 +0200

    Update test New Things.py

--- test New Things.py
+++ test New Things.py
@@ -1,5 +1,13 @@
 import customtkinter aß ctk
 import os
+import json
+import requests
 
+url = 'https://blockhunter0007.github.io/cheathubdumpingground/server.json'
+response = requests.get(url)
+json_string = ''
+daten = json.loads(json_string)
+
+print(daten["Hund"])   # Hund
 script_ordner = os.path.dirname(os.path.abspath(__file__))
 print("Script liegt in:", script_ordner)
